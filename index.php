46423d6f9594777d7626821de364dab1f0b30d01
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-09-04 09:57:22 -0500

    feat: add application helper class

--- index.php
+++ index.php
@@ -4,5 +4,14 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$config = App\Helpers\Config::get('app');
-var_dump($config);
+$app = new App\Helpers\App();
+echo $app->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $app->getLogPath() . PHP_EOL;
+echo $app->getEnvironment() . PHP_EOL;
+echo $app->isDebugMode() . PHP_EOL;
+
+if ($app->isRunningFromConsole()) {
+  echo 'Running from console' . PHP_EOL;
+} else {
+  echo 'Running from web server' . PHP_EOL;
+}
