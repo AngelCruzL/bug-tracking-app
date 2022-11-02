bd1328f47a48c9b7cdb268cf2836b42d4fa4fea8
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-11-02 12:14:52 -0600

    feat: add RepositoryInterface

---
+++ src/contracts/RepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+  namespace App\Contracts;
+
+  use App\Entity\Entity;
+
+  interface RepositoryInterface
+  {
+    public function find(int $id): ?object;
+    public function findOneBy(string $field, string $value): ?object;
+    public function findBy(array $criteria);
+    public function findAll(int $id): array;
+    public function sql(string $query);
+    public function create(Entity $entity): object;
+    public function update(Entity $entity, array $condition = []): object;
+    public function delete(Entity $entity, array $condition = []): void;
+  }
