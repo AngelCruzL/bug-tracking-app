21d31ec07a87d665e27b2a4076959f253fe51669
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-17 08:22:01 -0600

    feat: add docker service to simulate a production database

---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS bug_app;
+
+USE bug_app;
+
+CREATE TABLE IF NOT EXISTS reports
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    report_type VARCHAR(50)  NOT NULL,
+    message     TEXT         NOT NULL,
+    link        VARCHAR(255) NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
