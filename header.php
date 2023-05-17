2c80964330939c49eb9ae13ff387e38502eeaab0
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-17 08:23:23 -0600

    feat: display bug reports on browser app

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+  declare(strict_types=1);
+
+  require_once __DIR__ . '/vendor/autoload.php';
+  require_once __DIR__ . '/Src/Exception/exception.php';
+  require_once __DIR__ . '/Src/add.php';
+  require_once __DIR__ . '/Src/delete.php';
+  require_once __DIR__ . '/Src/read.php';
+  require_once __DIR__ . '/Src/update.php';
