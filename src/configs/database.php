7625754d4427064b20ff04092b1bd80f1f779945
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-16 22:25:14 -0600

    fix: database connection host credential

--- src/configs/database.php
+++ src/configs/database.php
@@ -3,7 +3,7 @@
 return [
   'pdo' => [
     'DB_DRIVER' => 'mysql',
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => '127.0.0.1',
     'DB_PORT' => '3306',
     'DB_NAME' => 'bug_app',
     'DB_USER' => 'root',
@@ -11,7 +11,7 @@ return [
     'default_fetch' => PDO::FETCH_OBJ,
   ],
   'mysqli' => [
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => '127.0.0.1',
     'DB_PORT' => '3306',
     'DB_NAME' => 'bug_app',
     'DB_USER' => 'root',
