3583546733ba770d125b1e9dcdb94e1ea002e197
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-17 10:08:23 -0600

    feat: add environmental variables support

--- src/configs/database.php
+++ src/configs/database.php
@@ -1,21 +1,25 @@
 <?php
 
-return [
-  'pdo' => [
-    'DB_DRIVER' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
-    'DB_PORT' => '3306',
-    'DB_NAME' => 'bug_app',
-    'DB_USER' => 'root',
-    'DB_PASS' => 'root',
-    'default_fetch' => PDO::FETCH_OBJ,
-  ],
-  'mysqli' => [
-    'DB_HOST' => '127.0.0.1',
-    'DB_PORT' => '3306',
-    'DB_NAME' => 'bug_app',
-    'DB_USER' => 'root',
-    'DB_PASS' => 'root',
-    'default_fetch' => MYSQLI_ASSOC,
-  ]
-];
+  require_once __DIR__ . '/../../vendor/autoload.php';
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+  $dotenv->safeLoad();
+
+  return [
+    'pdo' => [
+      'DB_DRIVER' => $_ENV['DB_DRIVER'],
+      'DB_HOST' => $_ENV['DB_HOST'],
+      'DB_PORT' => $_ENV['DB_PORT'],
+      'DB_NAME' => $_ENV['DB_NAME'],
+      'DB_USER' => $_ENV['DB_USER'],
+      'DB_PASS' => $_ENV['DB_PASS'],
+      'default_fetch' => PDO::FETCH_OBJ,
+    ],
+    'mysqli' => [
+      'DB_HOST' => $_ENV['DB_HOST'],
+      'DB_PORT' => $_ENV['DB_PORT'],
+      'DB_NAME' => $_ENV['DB_NAME'],
+      'DB_USER' => $_ENV['DB_USER'],
+      'DB_PASS' => $_ENV['DB_PASS'],
+      'default_fetch' => MYSQLI_ASSOC,
+    ]
+  ];
